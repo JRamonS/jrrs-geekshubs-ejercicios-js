8fe720ec09788c248f24f42dfdbeb0b1b4abdacc
Ramon <ramon.r.santana@gmail.com>
2023-01-31 20:56:57 +0100

    ejercicio-pseudocodigo-1-3

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*Ejercicio 1
+
+Diseñar el pseudocódigo de un programa que sume dos números leídos por
+pantalla y muestre el resultado por terminal.*/
+
+// let num1 = parseInt(prompt("Dame el primer numero"));
+// let num2 = parseInt(prompt("Dame el segundo numero"));
+
+// console.log("La suma es ", num1 + num2);
+
+/* Ejercicio 2
+
+Hacer el pseudocódigo de un programa que permita leer 2 números diferentes y
+nos diga cual es el mayor de los 2 números.*/
+
+// let num1 = parseInt(prompt("Dame el primer valor"));
+// let num2 = parseInt(prompt("Dame el segundo valor"));
+
+// if (num1 > num2){
+//     console.log(num1, "Es mayor");
+// }else{
+//     console.log(num2, "Es mayor")
+// }
+
+/* Ejercicio 3
+Crear el pseudocódigo de un programa que almacenen 3 números en 3 variables
+A, B y C. El diagrama debe decidir cuál es el mayor y cuál es el menor.*/ 
+
+// let num1 = parseInt(prompt("Dame el primer numero"));
+// let num2 = parseInt(prompt("Dame el segundo numero"));
+// let num3 = parseInt(prompt("Dame el tercero numero"));
+
+// if (num1 > num2 && num1 > num3){
+//     console.log(num1, "Es mayor");
+// }else if(num2 > num1 && num2 > num3){
+//     console.log(num2, "Es mayor");
+// }else{
+//     console.log(num3, "Es mayor")
+// }
